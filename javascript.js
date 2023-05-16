698a8a242245f5b2c0795aaa4bc055f72a56997d
jenny <jennytran9264@gmail.com>
2023-05-16 22:38:02 +1000

    Functioning grid

--- javascript.js
+++ javascript.js
@@ -1,35 +1,36 @@
-
+//Default grid settings, 16x16 grid with black ink.
 populateGrid(16)
-let color = "black"
+let color = "`hsla(180, 10%, 6%, 1)`"
 
+
+//Ideas
+//Bigger paint brush/ eraser
+//Going over the same tile over and over darkens it
 
 
 //Slider Function
 const output = document.getElementById("demo");
 const slider = document.getElementById("slider").oninput = function() {
     output.innerHTML = this.value + " x " + this.value; 
-    console.log(sliderValue)
     populateGrid(this.value)
 }
 
 //Erase Button
-    const eraseBtn = document.querySelector(".erase")
-    let sliderValue = document.getElementById("slider").oninput.value;
-
-    //Fix reset button
-
-
-    // eraseBtn.addEventListener('click', populateGrid(size));
-    
-    
+function resetGrid(){
+    let gridcontainer = document.querySelector(".gridcontainer")
+    let squares = gridcontainer.querySelectorAll("div")
+    squares.forEach((div) => div.style.backgroundColor = "white");
+}
+const eraseBtn = document.querySelector(".erase")
+eraseBtn.addEventListener("click", resetGrid)
 
 //When populateGrid() is run, all the squares are removed and repopulated
 //This removes any drawing on the previous grid
 function populateGrid(size) {
     //Default settings on the page
-    const gridcontainer = document.querySelector(".gridcontainer")
+    let gridcontainer = document.querySelector(".gridcontainer")
     //We want to select all the divs (div not .div) within the gridcontainer 
-    const squares = gridcontainer.querySelectorAll("div")
+    let squares = gridcontainer.querySelectorAll("div")
     squares.forEach((div) => div.remove());
     gridcontainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridcontainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -44,10 +45,38 @@ function populateGrid(size) {
          }
 } 
 
-
-
+//Selects which colour for the brush
+//Color is defaulted to black and is declared globally
+//The buttons change the colors via changeColor()
 function colorSquare(){
-    this.style.backgroundColor = color;
+    if(color === 'peach'){
+        //Math.random() gives a number between 0.1 to 1
+        //Color palattes are possible given their hsl numbers are consecutive
+        //Red - Yellow hsl1-54, green to dark green 72-133, blues 176-233, purples 258-330
+        this.style.backgroundColor = 
+        `hsl(${Math.random() * 54}, ${(Math.random() + 0.4) * 100 }%, 90%)`;
+    } else if (color === 'patty'){
+        this.style.backgroundColor = 
+        `hsl(${(Math.random() * 15) + 214}, ${(Math.random() + 0.2) * 100}%, ${(Math.random() * 75)}%)`;
+    } else if (color === 'nima'){
+        this.style.backgroundColor =
+        `hsl(240, 66%, ${(Math.random() * 20) + 68}%)`;
+    } else if (color === 'blackpink'){
+        this.style.backgroundColor =
+        randomBlackPink();
+    } else {
+        this.style.backgroundColor = color;
+    }
+}
+
+//Choosing a random colour from a palette rather than a color scheme using Math.random
+//Yes I am making this more complicated than needed
+function randomBlackPink() {
+    const options = [`#ff9898`, `#000000`, `#ff7b7b`, `#382d2d`, `#ff5f5f`]
+    let position = Math.floor(Math.random() * options.length)
+    randomiser = options[position];
+    console.log(randomiser);
+    return randomiser;
 }
 
 function changeColor(choice) {
