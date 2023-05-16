9e24fc6ccf6bf4d797ea5055891860be88a87387
jenny <jennytran9264@gmail.com>
2023-05-17 02:49:19 +1000

    decent css, need to enable toggle colours

--- javascript.js
+++ javascript.js
@@ -1,12 +1,11 @@
 //Default grid settings, 16x16 grid with black ink.
-populateGrid(16)
-let color = "`hsla(180, 10%, 6%, 1)`"
+populateGrid(8)
+let color = "peach"
 
 
 //Ideas
-//Bigger paint brush/ eraser
 //Going over the same tile over and over darkens it
-
+//toggle like feature on colours
 
 //Slider Function
 const output = document.getElementById("demo");
@@ -50,20 +49,23 @@ function populateGrid(size) {
 //The buttons change the colors via changeColor()
 function colorSquare(){
     if(color === 'peach'){
-        //Math.random() gives a number between 0.1 to 1
-        //Color palattes are possible given their hsl numbers are consecutive
-        //Red - Yellow hsl1-54, green to dark green 72-133, blues 176-233, purples 258-330
         this.style.backgroundColor = 
-        `hsl(${Math.random() * 54}, ${(Math.random() + 0.4) * 100 }%, 90%)`;
+        `hsl(${Math.random() * 54}, ${(Math.random() + 0.4) * 100}%, 90%)`;
+    } else if (color === 'sunflower'){
+        this.style.backgroundColor = 
+        `hsl(${(Math.random() * 20)+41}, ${(Math.random() + 0.5) * 100}%, ${(Math.random()*20)+55}%)`;
     } else if (color === 'patty'){
         this.style.backgroundColor = 
-        `hsl(${(Math.random() * 15) + 214}, ${(Math.random() + 0.2) * 100}%, ${(Math.random() * 75)}%)`;
+        `hsl(${(Math.random() * 15) + 214}, ${(Math.random() + 0.2) * 100}%, ${(Math.random()*75)}%)`;
     } else if (color === 'nima'){
         this.style.backgroundColor =
         `hsl(240, 66%, ${(Math.random() * 20) + 68}%)`;
     } else if (color === 'blackpink'){
         this.style.backgroundColor =
         randomBlackPink();
+    } else if (color ==='botanical'){
+        this.style.backgroundColor =
+        randomBotanical();
     } else {
         this.style.backgroundColor = color;
     }
@@ -72,7 +74,14 @@ function colorSquare(){
 //Choosing a random colour from a palette rather than a color scheme using Math.random
 //Yes I am making this more complicated than needed
 function randomBlackPink() {
-    const options = [`#ff9898`, `#000000`, `#ff7b7b`, `#382d2d`, `#ff5f5f`]
+    let options = [`#ff9898`, `#000000`, `#ff7b7b`, `#382d2d`, `#ff5f5f`]
+    let position = Math.floor(Math.random() * options.length)
+    randomiser = options[position];
+    console.log(randomiser);
+    return randomiser;
+}
+function randomBotanical() {
+    let options = [`#dad7cd`, `#a3b18a`, `#588157`, `#3a5a40`, `#344e41`]
     let position = Math.floor(Math.random() * options.length)
     randomiser = options[position];
     console.log(randomiser);
